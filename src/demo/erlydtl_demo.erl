e21125a525d1b55f19ef172db7f09d2f391e70db
Roberto Saccon <rsaccon@gmail.com>
2008-01-16 05:14:28 +0000

    added variable presetting, more tests and fixed if tag

--- src/demo/erlydtl_demo.erl
+++ src/demo/erlydtl_demo.erl
@@ -34,7 +34,7 @@
 -author('rsaccon@gmail.com').
 
 %% API
--export([compile_all/0, compile/1, compile/2, render_all/0, render/1, render/2, preset/1]).
+-export([compile_all/0, compile/1, compile/3, render_all/0, render/1, render/2]).
 
 %%====================================================================
 %% API
@@ -50,10 +50,9 @@ compile_all() ->
     filelib:fold_files(DocRoot,
         "\.html$|\.css$",
         true,
-        fun(Path, _Acc) ->
-            Module = filename:rootname(filename:basename(Path)),
-            % case erlydtl_server:compile(Path, DocRoot, Module, {?MODULE, preset}) of
-            case erlydtl_compiler:compile(Path, DocRoot, Module) of
+        fun(File, _Acc) ->
+            Module = filename:rootname(filename:basename(File)),
+            case erlydtl_compiler:compile(File, Module, DocRoot) of
                 ok ->
                     io:format("compile success: ~p~n",[Module]);
                 _ ->
@@ -70,49 +69,61 @@ compile_all() ->
 %% @end 
 %%--------------------------------------------------------------------        
 compile("var" = Name) ->
-    compile(Name, ".html");
+    compile(Name, ".html", []);
+    
+compile("var_preset" = Name) ->
+    Vars = [{preset_var1, "preset-var1"}, {preset_var2, "preset-var2"}],
+    compile(Name, ".html", Vars);
 
 compile("extends" = Name) ->
-    compile(Name, ".html"); 
+    compile(Name, ".html", []);
 
 compile("include" = Name) ->
-    compile(Name, ".html"); 
+    compile(Name, ".html", []);
 
 compile("autoescape" = Name) ->
-    compile(Name, ".html"); 
+    compile(Name, ".html", []);
 
 compile("if" = Name) ->
-    compile(Name, ".html"); 
+    compile(Name, ".html", []);
+    
+compile("if_preset" = Name) ->
+    Vars = [{var1, "something"}],
+    compile(Name, ".html", Vars);    
           
 compile("filters" = Name) ->
-    compile(Name, ".html"); 
+    compile(Name, ".html", []);
 
 compile("comment" = Name) ->
-    compile(Name, ".html");
+    compile(Name, ".html", []);
                
 compile("for" = Name) ->
-    compile(Name, ".html");
+    compile(Name, ".html", []);
  
 compile("for_records" = Name) ->
-    compile(Name, ".html");
+    compile(Name, ".html", []);
     
 compile("for_list" = Name) ->
-    compile(Name, ".html");
-                                
+    compile(Name, ".html", []);
+ 
+compile("for_list_preset" = Name) ->
+    Vars = [{fruit_list, [["apple", "apples"], ["banana", "bananas"], ["coconut", "coconuts"]]}],
+    compile(Name, ".html", Vars);
+                                      
 compile("htmltags" = Name) ->
-    compile(Name, ".html");
-                    
-compile("csstags" = Name) ->
-    compile(Name, ".css");
+    compile(Name, ".html", []);
 
-compile("var_preset" = Name) ->
-    compile(Name, ".html");
      
 compile("for_preset" = Name) ->
-    compile(Name, ".html");     
+    Vars = [{fruit_list, ["preset-apple", "preset-banana", "preset-coconut"]}],
+    compile(Name, ".html", Vars);     
 
 compile("for_records_preset" = Name) ->
-    compile(Name, ".html");
+    Link1 = [{name, "Amazon (preset)"}, {url, "http://amazon.com"}],
+    Link2 = [{name, "Google (preset)"}, {url, "http://google.com"}],
+    Link3 = [{name, "Microsoft (preset)"}, {url, "http://microsoft.com"}],
+    Var = [{software_links, [Link1, Link2, Link3]}],
+    compile(Name, ".html", Var);
           
 compile(Name) ->
     io:format("No such template: ~p~n",[Name]).
@@ -124,12 +135,11 @@ compile(Name) ->
 %% compiles the template to beam files
 %% @end 
 %%--------------------------------------------------------------------       
-compile(Name, Ext) ->
+compile(Name, Ext, Vars) ->
     DocRoot = filename:join([filename:dirname(code:which(?MODULE)),"..", "demo", "templates"]),
     Module = "test_" ++ Name,
-    Path = filename:join([DocRoot, Module ++ Ext]),
-    % case erlydtl_server:compile(Path, DocRoot, Module, {?MODULE, preset}) of
-    case erlydtl_compiler:compile(Path, DocRoot, Module) of
+    File = filename:join([DocRoot, Module ++ Ext]),
+    case erlydtl_compiler:compile(File, Module, DocRoot, Vars) of
         ok ->
             io:format("compile success: ~p~n",[Module]);
         _ ->
@@ -145,19 +155,20 @@ compile(Name, Ext) ->
 render_all() ->
     render("autoescape"),
     render("comment"),
-    render("csstags"),
     render("extends"),
     render("filters"),
     render("for"),
+    render("for_preset"),    
     render("for_list"),
-    %render("for_preset"),
+    render("for_list_preset"),
     render("for_records"),
-    %render("for_records_preset"),
+    render("for_records_preset"),
     render("htmltags"),
     render("if"),
+    render("if_preset"),        
     render("include"),
-    render("var").
-    %render("var_preset")
+    render("var"),
+    render("var_preset").
         
 
 %%--------------------------------------------------------------------
@@ -165,33 +176,39 @@ render_all() ->
 %% @doc renders template to a file
 %% @end 
 %%--------------------------------------------------------------------
-render("var" = Name) ->
-    render(Name, [{var1, "foostring1"}, {var2, "foostring2"}, {var_not_used, "foostring3"}]);
- 
+render("autoescape" = Name) ->
+    render(Name, [{var1, "<b>bold</b>"}]);
+  
+render("comment" = Name) ->
+    render(Name, []);
+          
+render("extends" = Name) ->
+    render(Name, [{base_var, "base-barstring"}, {test_var, "test-barstring"}]);
+    
 render("filters" = Name) ->
     render(Name, [{'list', ["eins", "zwei", "drei"]}]);
 
 render("include" = Name) ->
     render(Name, [{var1, "foostring1"}, {var2, "foostring2"}]);
  
-render("autoescape" = Name) ->
-    render(Name, [{var1, "<b>bold</b>"}]);
-
 render("if" = Name) ->
     render(Name, [{var1, "something"}]);
-
-render("extends" = Name) ->
-    render(Name, [{base_var, "base-barstring"}, {test_var, "test-barstring"}]);
-        
-render("comment" = Name) ->
+    
+render("if_preset" = Name) ->
     render(Name, []);
 
 render("for" = Name) ->
     render(Name, [{fruit_list, ["apple", "banana", "coconut"]}]);
+    
+render("for_preset" = Name) ->
+    render(Name, []);
             
 render("for_list" = Name) ->
     render(Name, [{fruit_list, [["apple", "apples"], ["banana", "bananas"], ["coconut", "coconuts"]]}]);
 
+render("for_list_preset" = Name) ->
+    render(Name, []);
+        
 render("for_records" = Name) ->
     Link1 = [{name, "Amazon"}, {url, "http://amazon.com"}],
     Link2 = [{name, "Google"}, {url, "http://google.com"}],
@@ -200,21 +217,18 @@ render("for_records" = Name) ->
                 
 render("htmltags" = Name) ->
     render(Name, []);
-    
-render("csstags" = Name) ->
-    render(Name, []);
-  
-render("var_preset" = Name) ->
-    render(Name, [{var1, "foostring1"}, {var2, "foostring2"}]);
- 
-render("for_preset" = Name) ->
-    render(Name, []);
-            
+             
 render("for_records_preset" = Name) ->
     Link1 = [{name, "Canon"}, {url, "http://canon.com"}],
     Link2 = [{name, "Leica"}, {url, "http://leica.com"}],
     Link3 = [{name, "Nikon"}, {url, "http://nikon.com"}],
     render(Name, [{photo_links, [Link1, Link2, Link3]}]);
+
+render("var" = Name) ->
+    render(Name, [{var1, "foostring1"}, {var2, "foostring2"}, {var_not_used, "foostring3"}]);
+
+render("var_preset" = Name) ->
+    render(Name, [{var1, "foostring1"}, {var2, "foostring2"}]);
         
 render(Name) ->
     io:format("No such template: ~p~n",[Name]).  
@@ -242,28 +256,7 @@ render(Name, Args) ->
             io:format("render failure: ~p ~p~n",[Module, Err])
     end.    
             
-
-%%--------------------------------------------------------------------
-%% @spec (atom()) -> proplist()
-%% @doc returns template preset variables
-%% @end 
-%%--------------------------------------------------------------------
-preset(test_extends) ->
-    [{preset_base_var, "preset-base-barstring"}];
-
-preset(test_var_preset) ->
-    [{preset_var1, "preset-var1"}, {preset_var2, "preset-var2"}];
-    
-preset(test_for_preset) ->
-    [{fruit_list, ["preset-apple", "preset-banana", "preset-coconut"]}];
-           
-preset(test_for_records_preset) ->
-    Link1 = [{name, "Amazon (preset)"}, {url, "http://amazon.com"}],
-    Link2 = [{name, "Google (preset)"}, {url, "http://google.com"}],
-    Link3 = [{name, "Microsoft (preset)"}, {url, "http://microsoft.com"}],
-    [{software_links, [Link1, Link2, Link3]}].
-
-              
+          
 %%====================================================================
 %% Internal functions
 %%====================================================================
