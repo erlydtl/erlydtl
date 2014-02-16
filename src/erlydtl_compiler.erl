4e10d5408777c4e6a24e06aa6e37680d4d2bfdf6
Сергей Прохоров <seriy.pr@gmail.com>
2014-02-17 01:37:29 +0400

    Drop unnecessary 3'rd argument of `erlydtl_runtime:translate/3`

--- src/erlydtl_compiler.erl
+++ src/erlydtl_compiler.erl
@@ -1250,7 +1250,7 @@ runtime_trans_ast(ValueAst, AstInfo, TreeWalker) ->
     StringLookupAst = erl_syntax:application(
                         erl_syntax:atom(erlydtl_runtime),
                         erl_syntax:atom(translate),
-                        [ValueAst, erl_syntax:variable("_TranslationFun"), ValueAst]), %TODO: remove 3'rd arg
+                        [ValueAst, erl_syntax:variable("_TranslationFun")]),
     {{StringLookupAst, AstInfo}, TreeWalker}.
 
 compiletime_trans_ast(String, AstInfo,
