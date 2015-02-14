cac9761a41e3459bc917d285a109bd216993ad29
danikp <dmitry.polyanovsky@gmail.com>
2015-02-14 22:32:56 +0200

    run dializer

---
+++ travis-dialyzer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PLT=plt/erlydtl-$RANDOM.plt
+echo "PLT File $PLT"
+export PATH=$PATH:/usr/local/bin:/usr/bin
+echo "Building PLT, may take a few minutes"
+dialyzer  --build_plt --apps kernel stdlib\
+       --output_plt $PLT > /dev/null
+
+echo "********************************************************************************"
+for app in  erts 
+do 
+    echo $"Adding $app"
+    dialyzer --add_to_plt --apps $app\
+       --plt $PLT > /dev/null
+done
+
+echo "********************************************************************************"
+for app in $(ls deps/)
+do
+   echo "Adding $app"
+   dialyzer --add_to_plt --apps deps/$app \
+       --plt $PLT > /dev/null
+done
+echo "********************************************************************************"
+echo ""
+
+dialyzer	ebin/			\
+    -Werror_handling		\
+    -Wrace_conditions		\
+    -Wunderspecs			\
+    -Wunmatched_returns	\
+    --verbose				\
+    --fullpath				\
+    -n						\
+    --plt $PLT
+#
